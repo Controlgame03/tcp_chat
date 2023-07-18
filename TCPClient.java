016f4bb2206b9e5a9348b87a6e5f765ec24984a5
denis <denserg765@gmail.com>
2023-07-18 15:40:20 +0300

    new version

--- TCPClient.java
+++ TCPClient.java
@@ -3,34 +3,44 @@ package TCPChat;
 import java.net.*;
 import java.io.*;
 import java.util.Date;
+import java.util.Scanner;
 
 /*
  * 4.07 клиент отправляет пакеты, но не получпет их
  * 
+ * 11.07 написать программу где клиент посылает сообщения а сервер их получает
+ * нужно сделать логи программы
  * */
 
 
 public class TCPClient {
 	public static void main(String[] argv) {
-		Socket clientSocket;
 		try {
+			Socket clientSocket;
 			String name = "denis";
-			
+			/*
 			InetAddress localHost = InetAddress.getByName(argv[0]);
 			InetAddress serverHost = InetAddress.getByName(argv[2]);
 			int localPort = Integer.parseInt(argv[1]);
 			int serverPort = Integer.parseInt(argv[3]);
+			*/
+			Scanner in = new Scanner(System.in);
 			
-			clientSocket = new Socket(serverHost, serverPort, localHost, localPort);
+			System.out.print("Введите ip адрес сервера: ");
+			InetAddress serverHost = InetAddress.getByName(in.next());
 			
-			Reciever reciever = new Reciever(clientSocket);
+			System.out.print("Введите порт на котором работает сервер: ");
+			int serverPort = Integer.parseInt(in.next());
+			
+			clientSocket = new Socket(serverHost, serverPort);
+			
+			System.out.println("\nУспешное соединение");
+			
 			Sender sender = new Sender(clientSocket, name);
-			reciever.start();
 			sender.start();
 		}
 		catch(Exception exep) {
 			System.out.println(exep.getMessage());
-			System.out.println(exep.getStackTrace());
 		}
 	}
 }
@@ -58,10 +68,12 @@ class Reciever extends Thread {
 		try {
 			/*Thread.sleep(10000);*/
 			while(true) {
-					String message;
-					if((message = in.readLine()) != null) {
-						System.out.println("recieve " + new Date() + " --->" + message);
-					}
+				//String message = in.readLine();
+				//System.out.print(message);
+				InputStreamReader myin = new InputStreamReader()
+				/*if((message = in.readLine()) != null) {
+					System.out.println("Получено сообщение " + new Date() + " --->" + message);
+				}*/
 				
 			}
 		}
@@ -74,8 +86,6 @@ class Reciever extends Thread {
 
 class Sender extends Thread {
 	Socket currentSocket;
-	InetAddress ipAddress;
-	int port;
 	String clientName;
 	
 	private static BufferedWriter out;
@@ -86,10 +96,7 @@ class Sender extends Thread {
 	            throw new Exception("Cannot open sender socket");
 	        }
 			currentSocket = socket;
-			this.ipAddress = socket.getInetAddress();
-			port = socket.getPort();
 			clientName = name;
-			
 			out = new BufferedWriter(new OutputStreamWriter(currentSocket.getOutputStream()));
 		}
 		catch(Exception excep) {
@@ -101,13 +108,15 @@ class Sender extends Thread {
 	public void run(){
 		try {
 			while(true) {	
-					BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-					String message;
-					if((message = buffer.readLine()) != null) {
-						System.out.println("send " + new Date() + " --->" + message);
-						String finalMessage = message;
-						out.write(finalMessage);
-					}
+				BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+				String message;
+				System.out.print("Введите сообщение: ");
+				if((message = buffer.readLine()) != null) {
+					message += "\n";
+					System.out.println("Отправлено сообщение " + new Date() + " ---> " + message);
+					String finalMessage = message;
+					out.write(finalMessage);
+				}
 				
 			}
 		}
--- TCPServer.java
+++ TCPServer.java
@@ -1,4 +1,48 @@
 package TCPChat;
-public class TCPServer {
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.util.Date;
+import java.util.Scanner;
+
+public class TCPServer {
+	public static void main(String[] argv) {		
+		try {
+			Scanner in = new Scanner(System.in);
+			Socket serverSocket;
+			
+			System.out.print("Введите порт на котором работает сервер: ");
+			int serverPort = Integer.parseInt(in.next());
+			
+			ServerSocket server = new ServerSocket(serverPort);
+			System.out.println("Ожидание подключения клиента...");
+			/*InetAddress clientHost = InetAddress.getByName(argv[0]);
+			InetAddress serverHost = InetAddress.getByName(argv[2]);
+			int clientPort = Integer.parseInt(argv[1]);
+			int serverPort = Integer.parseInt(argv[3]);*/
+			
+			//clientSocket = new Socket(serverHost, serverPort, localHost, localPort);
+			//serverSocket = new Socket(clientHost, clientPort, serverHost, serverPort);
+			
+			serverSocket = server.accept();
+			System.out.print("Подключился клиент. Ip адрес: ");
+			System.out.print(serverSocket.getInetAddress().toString());
+			System.out.print("; Порт: ");
+			System.out.println(serverSocket.getPort());
+			Reciever reciever = new Reciever(serverSocket);
+			//Sender sender = new Sender(serverSocket, name);
+			//reciever.start();
+			reciever.start();
+		}
+		catch(Exception exep) {
+			System.out.println(exep.getMessage());
+			System.out.println(exep.getStackTrace());
+		}
+	}
 }
+
